1e500991fbead18ec1983acdeb0224a76c2395b7
Jose Eduardo Serrano <joser_isi@hotmail.com>
2022-03-23 19:15:35 -0600

    primer proyecto

---
+++ session1/Program.cs
@@ -0,0 +1,23 @@
+﻿
+using System; // Librerias que se utilizan en el programa 
+using System.Collections;
+
+namespace session1  // Espacio de nombres 
+{
+    public class Program 
+    {
+        static void Main ()
+        {
+            Console.WriteLine ("Ingresa un numero");
+            int input = 10;
+            var resultado = sumar(input);            
+            Console.WriteLine ("El resultado es {0}", resultado);
+        }
+
+        static int sumar(int input)
+        {
+            int resultado = input + input;
+            return resultado;
+        }
+    }
+}
