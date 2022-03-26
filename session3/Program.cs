f89dea7cdcfbfd234c4db702173985301e786d62
Jose Eduardo Serrano <joser_isi@hotmail.com>
2022-03-25 19:09:25 -0600

    se agrego sesion 2

---
+++ session3/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+bool isTrue = true;
+
+if(isTrue)
+{
+    Console.WriteLine("Es verdad");
+}
+else
+{
+    Console.WriteLine("Es mentira");
+}
+
+
+for(int i = 0; i <100; i++)
+{
+   // Console.WriteLine(i);
+}
+
+int x = 0;
+do 
+{
+    
+    //Console.WriteLine("Hacer mientras que x < 100");
+    x++;
+}while(x < 100);
+
+int y= 0;
+while(y < 100)
+{
+    //Console.WriteLine("Hacer mientras que y < 100");
+    y++;
+}
