cc3057210448a78da55305b38dc54a4cfcb89643
Jose Eduardo Serrano <joser_isi@hotmail.com>
2022-05-06 18:36:37 -0500

    add pending sessions

---
+++ session15/Program.cs
@@ -0,0 +1,90 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+int [] arreglo = new int [10] {1,2,3,4,5,6,7,8,9,10};
+
+
+var nQuery = 
+    from item in arreglo
+    where item % 2 == 0
+    select item;
+
+foreach (var item in nQuery)
+{
+    Console.WriteLine(item);
+}
+
+var nQueryMayor5 = 
+    from item in arreglo
+    where item > 5
+    select item;
+
+foreach (var item in nQueryMayor5)
+{
+    Console.WriteLine(item);
+}   
+
+Console.WriteLine("\n\n");
+List<string> listado = new List<string>();  
+listado.Add("Hola");
+listado.Add("Mundo");
+listado.Add("ayer");
+listado.Add("tambien");
+
+var squery = 
+    from item in listado
+    where item.Length > 5
+    select item;
+
+foreach (var item in squery)
+{
+    Console.WriteLine(item);
+}
+
+
+List<Auto> listadoAutos = new List<Auto>();
+
+listadoAutos.Add(new Auto("Ford", "Fiesta", 80000));
+listadoAutos.Add(new Auto("Chevrolet", "Silverado", 120000));
+listadoAutos.Add(new Auto("Nissan", "Tsuru", 20000));
+listadoAutos.Add(new Auto("Nissan", "Versa", 150000));
+
+/*
+var nQueryAutos = 
+    from item in listadoAutos
+    where item.Marca == "Nissan"
+    select item;*/
+
+var nQueryAutos2 = 
+    from item2 in listadoAutos
+    where item2.Precio > 130000
+    select new { item2.Marca, item2.Modelo };
+/*
+foreach (var item in nQueryAutos)
+{
+    Console.WriteLine(item.Marca + " " + item.Modelo + " " + item.Precio + " " );
+}*/
+
+foreach (var item in nQueryAutos2)
+{
+    Console.WriteLine(item.Marca + " " + item.Modelo);
+}
+
+
+
+
+
+
+class Auto
+{
+    public Auto(string marca, string modelo, decimal precio)
+    {
+        Marca = marca;
+        Modelo = modelo;
+        Precio = precio;
+    }
+    public string Marca { get; set; }
+    public string Modelo { get; set; }
+    public decimal Precio { get; set; }
+}
---
+++ session16/Program.cs
@@ -0,0 +1,45 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+ List<Item_mast> itemlist = new List<Item_mast>
+	        {  
+           new Item_mast { ItemId = 1, ItemDes = "Biscuit  " }, 
+           new Item_mast { ItemId = 2, ItemDes = "Chocolate" }, 
+           new Item_mast { ItemId = 3, ItemDes = "Butter   " },  
+           new Item_mast { ItemId = 4, ItemDes = "Brade    " },  
+           new Item_mast { ItemId = 5, ItemDes = "Honey    " }  
+            }; 
+
+    List<Purchase> purchlist = new List<Purchase>
+	        {  
+           new Purchase { InvNo=100, ItemId = 3,  PurQty = 800 }, 
+           new Purchase { InvNo=101, ItemId = 2,  PurQty = 650 }, 
+           new Purchase { InvNo=102, ItemId = 3,  PurQty = 900 },  
+           new Purchase { InvNo=103, ItemId = 4,  PurQty = 700 },
+		   new Purchase { InvNo=104, ItemId = 3,  PurQty = 900 },  
+           new Purchase { InvNo=105, ItemId = 4,  PurQty = 650 },  		   
+           new Purchase { InvNo=106, ItemId = 1,  PurQty = 458 }  
+            }; 
+        
+    var innerJoin = from e in itemlist 
+        join d in purchlist 
+        on e.ItemId equals d.ItemId
+        select new { e.ItemId, e.ItemDes, d.PurQty };
+        
+    foreach (var item in innerJoin)
+    {
+        Console.WriteLine("{0} {1} {2}", item.ItemId, item.ItemDes, item.PurQty);
+    }
+
+public class Item_mast
+{
+    public int ItemId { get; set; }
+    public string ItemDes { get; set; }
+}
+
+public class Purchase
+{
+    public int InvNo { get; set; }
+    public int ItemId { get; set; }
+    public int PurQty { get; set; }
+}
