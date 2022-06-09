0ca0787228ea42a4598c82d4b45f894a6cb07839
Jose Eduardo Serrano <joser_isi@hotmail.com>
2022-06-08 19:13:01 -0500

    Agregar cambios a la session 22

--- session22/MovieConsole/Program.cs
+++ session22/MovieConsole/Program.cs
@@ -3,13 +3,15 @@
 var repository = new Repository();
 repository.LoadFile(@"C:\Users\joser\OneDrive\Documentos\TRSF\Spring 2022\movies.csv");
 Console.WriteLine("Se cargo el archivo");
-/*Console.WriteLine("Titulo    --    Genero  --- Idioma");
+Console.WriteLine("Titulo    --    Genero  --- Idioma");
 foreach (var movie in repository.GetAll().Take(10))
 {
     Console.WriteLine($"{movie.Title}    --    {movie.Genre}  ---    {movie.OriginalLanguage}");
-}*/
+}
  
 
  IFileWriter writer = new FileWriterJson();
  writer.Write(repository.GetAll().Take(50).ToList(), "movies.json");
+
+Console.Write("Archivo modificado");
  
